ca1a1750d83932988eb102d3f5757b3ec2b411a4
Mike Burkle <maburkle@gmail.com>
2019-01-06 03:18:48 -0500

    Add inital sorting by card number

--- app/controllers/card_sets_controller.rb
+++ app/controllers/card_sets_controller.rb
@@ -6,6 +6,6 @@ class CardSetsController < ApplicationController
 
 	def show
 		@set = CardSet.find(params[:id])
-		@cards = @set.cards
+		@cards = @set.cards.sort_by { |c| c.card_number.scan(/\d+/).first.to_i }
 	end
 end
